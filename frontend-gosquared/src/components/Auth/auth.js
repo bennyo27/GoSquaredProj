7817fe407b5d7090ccf7c2c7f45add6629d10163
benny <oseguerabenny27@gmail.com>
2019-09-07 15:39:44 -0700

    Setup: Backend

--- frontend-gosquared/src/components/Auth/auth.js
+++ frontend-gosquared/src/components/Auth/auth.js
@@ -4,6 +4,8 @@ import "../../styles/css/auth.css";
 
 class Auth extends Component {
   state = {
+    usename: "",
+    password: "",
     registerFlag: false
   };
 
@@ -13,6 +15,10 @@ class Auth extends Component {
     });
   };
 
+  register = () => {};
+
+  login = () => {};
+
   componentDidUpdate() {
     console.log(this.state.registerFlag);
   }
@@ -20,12 +26,51 @@ class Auth extends Component {
   render() {
     return (
       <div className="auth-wrapper">
-        <div className="header">GoSquared</div>
+        <div className="header">
+          <div className="image">
+            <img
+              className="logo"
+              src="https://static.gosquared.com/images/nav/logo.png"
+            />
+          </div>
+        </div>
         <div className="content">
-          <div className="form"></div>
+          <div className="form">
+            <form
+              onSubmit={
+                this.state.registerFlag ? this.registerUser : this.loginUser
+              }
+            >
+              <input
+                className="text"
+                type="text"
+                name="username"
+                value={this.state.username}
+                placeholder="Username"
+              />
+
+              <input
+                className="text"
+                type="password"
+                name="password"
+                value={this.state.password}
+                placeholder="Password"
+              />
+
+              {this.state.registerFlag ? (
+                <input className="button" type="submit" value="Register" />
+              ) : (
+                <input className="button" type="submit" value="Login" />
+              )}
+            </form>
+          </div>
         </div>
         <div className="switch">
-          <button onClick={this.handleRegister}>Register Here</button>
+          <button onClick={this.handleRegister}>
+            {this.state.registerFlag
+              ? "Click here to login!"
+              : " Click here to register!"}
+          </button>
         </div>
       </div>
     );
